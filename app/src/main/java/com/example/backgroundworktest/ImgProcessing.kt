bccad068626231a0f189722553c9337f4b0df30c
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-02-19 02:01:57 +0900

    [main] 코루틴 구현

---
+++ app/src/main/java/com/example/backgroundworktest/ImgProcessing.kt
@@ -0,0 +1,42 @@
+package com.example.backgroundworktest
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.widget.ImageView
+import android.widget.LinearLayout
+import kotlinx.coroutines.CoroutineScope
+import java.util.concurrent.TimeUnit
+
+object ImgProcessing {
+
+    private lateinit var image: Bitmap
+     fun addImg (context : Context) : ImageView{
+         val imageView = ImageView(context)
+
+        try {
+
+
+            val assetsManger = context.resources.assets
+            val inputStream =  assetsManger.open("image.png")
+            image= BitmapFactory.decodeStream(inputStream)
+
+            imageView.apply {
+                layoutParams = LinearLayout.LayoutParams(
+                    100,
+                    100
+                )
+            }
+
+            imageView.setImageBitmap(image)
+
+
+            TimeUnit.SECONDS.sleep(1)
+
+        }catch(e : Exception) {
+            e.printStackTrace()
+        }
+
+         return imageView
+    }
+}
--- app/src/main/java/com/example/backgroundworktest/MainActivity.kt
+++ app/src/main/java/com/example/backgroundworktest/MainActivity.kt
@@ -1,26 +1,97 @@
 package com.example.backgroundworktest
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.Gravity
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainFrame : LinearLayout
+
+
+    private lateinit var image: Bitmap
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val mainFrame = LinearLayout(this)
 
-        mainFrame.apply {
+        mainFrame = LinearLayout(this).apply {
             layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER
             setBackgroundColor(Color.WHITE)
         }
 
+        val btn: Button = Button(this)
+        btn.apply {
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            setBackgroundColor(Color.GRAY)
+            text = "클릭"
+        }
+        mainFrame.addView(btn)
+
+
+        val assetsManger = this.resources.assets
+        val inputStream = assetsManger.open("image.png")
+        image = BitmapFactory.decodeStream(inputStream)
+
+
         setContentView(mainFrame)
+
+        btn.setOnClickListener {
+
+            Toast.makeText(this, "tap", Toast.LENGTH_SHORT).show()
+
+            GlobalScope.launch(Dispatchers.IO) {
+                for (i in 0..4) {
+
+                    try {
+                        addImg()
+                        delay(1000L)
+
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                }
+            }
+        }
     }
+
+        private suspend fun addImg () = withContext(Dispatchers.Main) {
+
+            try {
+                val imageView = ImageView(this@MainActivity)
+                imageView.setImageBitmap(image)
+                mainFrame.addView(imageView)
+
+
+
+            }catch(e : Exception) {
+                e.printStackTrace()
+            }
+        }
+
 }
+
+
+
+
